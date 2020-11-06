0595d3502ae409e695529cfb532030007139833e
Asmit <bhantanasmit@gmail.com>
2020-11-06 06:04:36 +0545

    android mvvm setup

---
+++ app/src/main/java/org/ak/nfn/ui/auth/LoginActivity.kt
@@ -0,0 +1,12 @@
+package org.ak.nfn.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.ak.nfn.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/org/ak/nfn/ui/auth/SignupActivity.kt
@@ -0,0 +1,12 @@
+package org.ak.nfn.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.ak.nfn.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
