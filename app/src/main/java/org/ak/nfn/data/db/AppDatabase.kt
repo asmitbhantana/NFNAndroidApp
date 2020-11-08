69a311210d73ef639fcad9d29725351f0a807df0
Asmit <bhantanasmit@gmail.com>
2020-11-08 08:24:36 +0545

    room database setup for user information saving after logging in

---
+++ app/src/main/java/org/ak/nfn/data/db/AppDatabase.kt
@@ -0,0 +1,34 @@
+package org.ak.nfn.data.db
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import org.ak.nfn.data.db.entities.User
+
+@Database(
+    entities = [User::class],
+    version = 1
+)
+abstract class AppDatabase: RoomDatabase() {
+    abstract fun getUserDao(): UserDao
+
+    companion object {
+        //immediately visible to other threads
+        @Volatile
+        private var instance: AppDatabase? = null
+        private val LOCK = Any()
+
+        //called on the creating of object
+        operator  fun invoke(context: Context) = instance ?: synchronized(LOCK){
+            instance?:buildDatabase(context).also {
+                instance = it
+            }
+        }
+
+        private fun buildDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext, AppDatabase::class.java, "Mydatabase.db" ).build()
+
+
+    }
+}
---
+++ app/src/main/java/org/ak/nfn/data/db/UserDao.kt
@@ -0,0 +1,20 @@
+package org.ak.nfn.data.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import org.ak.nfn.data.db.entities.CURRENT_USER_ID
+import org.ak.nfn.data.db.entities.User
+
+@Dao
+interface UserDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun upsert(user: User) : Long
+
+    @Query("Select * from user where uid = $CURRENT_USER_ID")
+    fun getUser(): LiveData<User>
+
+}
